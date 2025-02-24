cd5ee4990e5156a76b9d9e37c86cd03b0b137aa0
thinkpad <2190036445@qq.com>
2025-02-24 17:47:07 +0800

    2.24 17:47

--- chap4/4.4.1.py
+++ chap4/4.4.1.py
@@ -1,6 +1,15 @@
-import paddle.nn
-import paddle.nn as nn
-import paddle.nn.functional as F
-from paddle.nn.initializer import Constant,Normal,Uniform
+import paddle
 
-class Model_MLP_L2_V4(paddle.nn.Layer)
+# 2个特征数为5的样本
+X = paddle.rand(shape=[2, 5])
+
+# 含有5个参数的权重向量
+w = paddle.rand(shape=[5, 1])
+# 偏置项
+b = paddle.rand(shape=[1, 1])
+
+# 使用'paddle.matmul'实现矩阵相乘
+z = paddle.matmul(X, w) + b
+print("input X:", X)
+print("weight w:", w, "\nbias b:", b)
+print("output z:", z)
